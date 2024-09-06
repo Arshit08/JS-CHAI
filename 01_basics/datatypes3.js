66acc0e8130dfd38cca023b9ab5417f01ca48a17
Arshit08 <159623183+Arshit08@users.noreply.github.com>
2024-09-06 09:02:18 +0000

    day2

---
+++ 01_basics/datatypes3.js
@@ -0,0 +1,20 @@
+// let heros =["ironman","aquaman","batman"]
+
+// console.log(heros);
+// console.log(typeof heros);
+
+// let myobj = {
+//     Name : "abcd",
+//     age : 22
+// }
+
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+function test()
+    {
+        console.log("zinda hu yaaro")
+}
+console.log(typeof test)
